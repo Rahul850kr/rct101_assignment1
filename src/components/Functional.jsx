dff2ef1e725f5193c2a954f54bfcea45c9e86e38
Rahul kumar <rahul850kr@gmail.com>
2022-05-19 22:21:27 +0530

    update

---
+++ src/components/Functional.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Functional = ()=>{
+    return (
+        <div>
+            <h2>Mobile Operating System</h2>
+            <ul>
+                <li>Android</li>
+                <li>Blackberry</li>
+                <li>iphone</li>
+                <li>Windows Phone</li>
+            </ul>
+           
+            <h2>Mobile Manufacturers</h2>
+            <ul type="circle">
+                <li type="square">Samsung</li>
+                <li type="square">HTC</li>
+                <li type="disc">Micromax</li>
+                <li type="circle">Apple</li>
+            </ul>
+            
+        </div>
+    );
+    
+}
+
+export default Functional;
